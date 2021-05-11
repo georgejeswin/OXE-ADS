7eedabf832dfcd3115864cbe5a395ea74d3d6578
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-12 01:16:07 +0530

    form added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,6 +3,7 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
+    <>
     <div className="contact">
       <div className="contact-container">
        
@@ -33,7 +34,7 @@ const Contact = () => {
     <div class="drop drop-2"></div>
     <div class="drop drop-3"></div>
     <div class="drop drop-4"></div>
-    <div class="drop drop-5"></div>
+    {/* <div class="drop drop-5"></div> */}
             </div>
    
        </div>
@@ -41,7 +42,26 @@ const Contact = () => {
           </div>
          
         </div>
+       <div className="contact-form">
+       <div className="card card-2">
+       <input type="text" placeholder="Name"/>
+       <input type="email" placeholder="Email"/>
+       <input type="number" placeholder="Phone"/>
+       <textarea type="text" rows="6"  placeholder="Messages">
+      </textarea>
+      <button className="btn-2"><span>Send</span><i class="fas fa-paper-plane"></i></button>
+          <div class="drops">
+    <div class="drop drop-5"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-7"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-6"></div>
+            </div>
+            </div>
+       </div>
+       
       
+      </>
     
   );
 };
