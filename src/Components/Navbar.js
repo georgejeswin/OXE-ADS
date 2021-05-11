486744b6779547a7282d6260ff0d99a99a72bc83
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-12 04:15:34 +0530

    hanburder added and make some responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,36 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import {Link} from 'react-router-dom'
 const NavBar = () => {
+    const [button,setButton] =  useState(true)
+    const [click,setClick] = useState(false)
+    const handleClick =()=>{
+       setClick(!click)
+    }
+    const closeMobileMenu=()=>{
+        setClick(false)
+       }
+    const showButton =()=>{
+        if(window.innerWidth <= 980){
+          setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+    useEffect(()=>{
+        showButton()
+    },[])
+    window.addEventListener('resize',showButton)
     return (
         <>
          <nav className="navbar">
              <div className="nav-container">
-               <Link to="/" className="nav-logo"><img className="logo" alt="logo" src="./logo/logo-1.png"/></Link>
-             <ul className='nav-menu'>
+             <div className="navMobile">
+             <Link to="/" className="nav-logo" onClick={closeMobileMenu}><img className="logo" alt="logo" src="./logo/logo-1.png"/></Link>
+               <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+             </div>
+            
+             <ul className={click ? 'nav-menu active':'nav-menu'}>
                  <li className="nav-item">
-                     <Link to='/' className="nav-links">HOME</Link>
+                     <Link to='/' className="nav-links" onClick={closeMobileMenu}>HOME</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to='/' className="nav-links">SERVICES</Link>
+                 <Link to='/' className="nav-links" onClick={closeMobileMenu}>SERVICES</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to='/' className="nav-links">PROCESS</Link>
+                 <Link to='/' className="nav-links" onClick={closeMobileMenu}>PROCESS</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to='/' className="nav-links">WORK</Link>
+                 <Link to='/' className="nav-links" onClick={closeMobileMenu}>WORK</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to='/' className="nav-links">CLIENTS</Link>
+                 <Link to='/' className="nav-links" onClick={closeMobileMenu}>CLIENTS</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to='/' className="nav-links">CONTACTS</Link>
+                 <Link to='/' className="nav-links" onClick={closeMobileMenu}>CONTACTS</Link>
                  </li>
                  <li className="nav-item">
-                 <Link to="/" className="btn btn-2 btn-3 " >
+                 {button && <Link to="/" className="btn btn-3 " >
                 <span>8289906276</span> 
                 <i class="fas fa-phone"></i>
-          </Link>
+          </Link>  }
+                 
                  </li>
              </ul>
 
