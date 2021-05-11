0a963df5ce1f39a8a709d3e0d4e73aca46038249
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-11 05:41:21 +0530

    main menu completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,11 @@ const Home = () => {
         <>
         <NavBar />
         <div className="home">
-            
+            <div className="home-container">
+                <img src="./logo/head.png" alt="head"  className="head-img" />
+                <h4 className="main-text">We are</h4>
+                <h1 className="main-head">A Creative Digital Studio</h1>
+            </div>
         </div>
         </>
     )
