daa5219c2797d0d51bbd2f77b612b75943f1f545
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-11 22:30:10 +0530

    contact card added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,14 +5,44 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <div className="contact-head">
-          <h1>Contact Us</h1>
-        </div>
+       
         <div className="contact-content">
           <img src="./logo/contact.png" alt="" />
         </div>
-      </div>
-    </div>
+        <div className="contact-head">
+          <h1>Contact Us</h1>
+         
+          <div className="card">
+              <div className="card-text">
+                <h1>New Biz</h1>
+                <p>Oxeadsmedia@gmail.com</p>
+                <p>+8289906276</p>
+              </div>  
+              <div className="card-text">
+                <h1>General</h1>
+                <p>Oxeadsmedia@gmail.com</p>
+                <p>+8289906276</p>
+              </div>  
+              <div className="card-text">
+                <h1>Career</h1>
+                <p>Oxeadsmedia@gmail.com</p>
+                <p>+8289906276</p>
+              </div>  
+          <div class="drops">
+    <div class="drop drop-1"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-3"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-5"></div>
+            </div>
+   
+       </div>
+</div>
+          </div>
+         
+        </div>
+      
+    
   );
 };
 
