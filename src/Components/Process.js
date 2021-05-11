e254d24960b37729c7d55c39c737c1212beb3cf9
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-11 16:31:39 +0530

    animation added

--- src/Components/Process.js
+++ src/Components/Process.js
@@ -1,5 +1,26 @@
 import React from "react";
 import "./Process.css";
+import AOS from 'aos';
+AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 const Process = () => {
   return (
     <div className="process">
@@ -9,12 +30,12 @@ const Process = () => {
         </div>
         <div className="menu">
           <ul className="menu-lists">
-            <li className="menu-list">Pick Your Favourite Creative Services</li>
-            <li className="menu-list">Pay Using Our Secure Gateway</li>
-            <li className="menu-list">
+            <li className="menu-list" data-aos="fade-right">Pick Your Favourite Creative Services</li>
+            <li className="menu-list" data-aos="fade-right">Pay Using Our Secure Gateway</li>
+            <li className="menu-list" data-aos="fade-right">
               Give Us A Brief About What You Need Done
             </li>
-            <li className="menu-list">
+            <li className="menu-list" data-aos="fade-right">
               Sit Back While We Deliver Your Creatives
             </li>
           </ul>
