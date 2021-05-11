f1fe575aab67e73732f69fd21629ed2415e09748
georgejeswin <jeswinmyladoor@gmail.com>
2021-05-12 01:26:45 +0530

    mobile responsive

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,44 +5,39 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-       
-        <div className="contact-content">
+        <div className="contact-content res-hide">
           <img src="./logo/contact.png" alt="" />
         </div>
         <div className="contact-head">
           <h1>Contact Us</h1>
-         
+
           <div className="card">
-              <div className="card-text">
-                <h1>New Biz</h1>
-                <p>Oxeadsmedia@gmail.com</p>
-                <p>+8289906276</p>
-              </div>  
-              <div className="card-text">
-                <h1>General</h1>
-                <p>Oxeadsmedia@gmail.com</p>
-                <p>+8289906276</p>
-              </div>  
-              <div className="card-text">
-                <h1>Career</h1>
-                <p>Oxeadsmedia@gmail.com</p>
-                <p>+8289906276</p>
-              </div>  
-          <div class="drops">
-    <div class="drop drop-1"></div>
-    <div class="drop drop-2"></div>
-    <div class="drop drop-3"></div>
-    <div class="drop drop-4"></div>
-    <div class="drop drop-5"></div>
+            <div className="card-text">
+              <h1>New Biz</h1>
+              <p>Oxeadsmedia@gmail.com</p>
+              <p>+8289906276</p>
             </div>
-   
-       </div>
-</div>
+            <div className="card-text">
+              <h1>General</h1>
+              <p>Oxeadsmedia@gmail.com</p>
+              <p>+8289906276</p>
+            </div>
+            <div className="card-text">
+              <h1>Career</h1>
+              <p>Oxeadsmedia@gmail.com</p>
+              <p>+8289906276</p>
+            </div>
+            <div class="drops">
+              <div class="drop drop-1"></div>
+              <div class="drop drop-2"></div>
+              <div class="drop drop-3"></div>
+              <div class="drop drop-4"></div>
+              <div class="drop drop-5"></div>
+            </div>
           </div>
-         
         </div>
-      
-    
+      </div>
+    </div>
   );
 };
 
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -10,86 +10,94 @@ const Work = () => {
         </div>
         <div className="work-contents">
           <div className="work-content">
-            <div className="work-content-left">
-              <div className="work-content-text">
-                <h1 >E-Commerce </h1>
-                <p >
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make
-                  a type specimen book.
-                </p>
+            <h1>E-Commerce </h1>
+            <div className="work-content-item">
+              <div className="work-content-left res-hide">
+                <div className="work-content-text">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="work-content-right" >
-              <img data-aos="zoom-in" src="./logo/work-ecom.png" alt=""  />
-            </div>
-          </div>
-          <div className="work-content">
-            <div className="work-content-left">
-              <img data-aos="zoom-in" src="./logo/tech.png" alt="" />
-            </div>
-            <div className="work-content-right">
-              <div className="work-content-text">
-                <h1>Technology</h1>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make
-                  a type specimen book.
-                </p>
+              <div className="work-content-right ">
+                <img data-aos="zoom-in" src="./logo/work-ecom.png" alt="" />
               </div>
             </div>
           </div>
           <div className="work-content">
-            <div className="work-content-left">
-              <div className="work-content-text">
-                <h1>Travel & Tourism</h1>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make
-                  a type specimen book.
-                </p>
+            <h1>Technology</h1>
+            <div className="work-content-item">
+              <div className="work-content-left">
+                <img data-aos="zoom-in" src="./logo/tech.png" alt="" />
               </div>
-            </div>
-            <div className="work-content-right">
-              <img data-aos="zoom-in" src="./logo/travel.png" alt="" />
+              <div className="work-content-right res-hide">
+                <div className="work-content-text">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="work-content">
-            <div className="work-content-left">
-              <img data-aos="zoom-in" src="./logo/media.png" alt="" />
+            <h1>Travel & Tourism</h1>
+            <div className="work-content-item">
+              <div className="work-content-left res-hide">
+                <div className="work-content-text">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book.
+                  </p>
+                </div>
+              </div>
+              <div className="work-content-right">
+                <img data-aos="zoom-in" src="./logo/travel.png" alt="" />
+              </div>
             </div>
-            <div className="work-content-right">
-              <div className="work-content-text">
-                <h1>Media</h1>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make
-                  a type specimen book.
-                </p>
+          </div>
+          <div className="work-content">
+            <h1>Media</h1>
+            <div className="work-content-item">
+              <div className="work-content-left">
+                <img data-aos="zoom-in" src="./logo/media.png" alt="" />
+              </div>
+              <div className="work-content-right res-hide">
+                <div className="work-content-text">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
