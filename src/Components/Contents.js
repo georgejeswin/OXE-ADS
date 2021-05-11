10b42e6028ada289041b9d559aeda58718ebafd0
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-11 11:09:26 +0530

    proces section finished

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Contents.css'
+import Process from './Process'
 import Services from './Services'
 const Contents = () => {
     return (
         <div className="contents">
             <Services/>
+            <Process/>
         </div>
     )
 }
---
+++ src/Components/Process.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Process.css'
+const Process = () => {
+    return (
+        <div className="process">
+        <div className="process-container">
+            <div className="head">
+                <h1>How We Work.</h1>
+            </div>
+            <div className="menu">
+                <ul className="menu-lists">
+                    <li className="menu-list">Pick Your Favourite Creative Services</li>
+                    <li className="menu-list">Pay Using Our Secure Gateway</li>
+                    <li className="menu-list">Give Us A Brief About What
+You Need Done</li>
+                    <li className="menu-list">Sit Back While We Deliver Your
+Creatives</li>
+                </ul>
+                <img src="./logo/process.png" alt="process imgage" />
+            </div>
+        </div>
+            
+        </div>
+    )
+}
+
+export default Process
