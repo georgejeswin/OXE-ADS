a2670a11e25182c4679657e6d47d96d707638adb
georgejeswin <jeswinmyladoor@gmail.com>
2021-05-11 15:20:23 +0530

    footer styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,30 +9,43 @@ const Footer = () => {
         <Link to="/" className="footer-logo">
           <img className="logo" alt="logo" src="./logo/logo-1.svg" />
         </Link>
-        <ul className="footer-menu">
-          <li className="footer-item">
-            <Link to="/" className="footer-links">
-              HOME
-            </Link>
-          </li>
-          <li className="footer-item">
-            <Link to="/" className="footer-links">
-              SERVICES
-            </Link>
-          </li>
-          <li className="footer-item">
-            <Link to="/" className="footer-links">
-              CLIENTS
-            </Link>
-          </li>
-          <li className="footer-item">
-            <Link to="/" className="footer-links">
-              CONTACTS
-            </Link>
-          </li>
-        </ul>
+        <div className="footer-menus">
+          <ul className="footer-menu">
+            <li className="footer-item">
+              <Link to="/" className="footer-links">
+                HOME
+              </Link>
+            </li>
+            <li className="footer-item">
+              <Link to="/" className="footer-links">
+                SERVICES
+              </Link>
+            </li>
+            <li className="footer-item">
+              <Link to="/" className="footer-links">
+                CLIENTS
+              </Link>
+            </li>
+            <li className="footer-item">
+              <Link to="/" className="footer-links">
+                CONTACTS
+              </Link>
+            </li>
+          </ul>
+          <p>
+            Privacy Policy &nbsp; &#8226; &nbsp; Terms & Conditions &nbsp;
+            &#8226; &nbsp; Security
+          </p>
+        </div>
+
         <div className="footer-social">
-          <h2>Our Social Network</h2>
+          <h2>Our Social Networks:</h2>
+          <p className="footer-bottom-social">
+            <i class="fab fa-instagram fa-lg"></i> &nbsp;
+            <i class="fab fa-facebook-f fa-lg"></i> &nbsp;
+            <i class="fab fa-youtube fa-lg"></i> &nbsp;
+            <i class="fab fa-google-plus-g fa-lg"></i>
+          </p>
         </div>
       </div>
     </footer>
