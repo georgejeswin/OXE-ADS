524c0afdcfd4aeaa44c3072c9d50f00d2b92b213
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-11 16:34:23 +0530

    process animation added

--- src/Components/Process.js
+++ src/Components/Process.js
@@ -25,17 +25,17 @@ const Process = () => {
   return (
     <div className="process">
       <div className="process-container">
-        <div className="head">
+        <div className="head"  data-aos="fade-up">
           <h1>How We Work.</h1>
         </div>
         <div className="menu">
           <ul className="menu-lists">
-            <li className="menu-list" data-aos="fade-right">Pick Your Favourite Creative Services</li>
-            <li className="menu-list" data-aos="fade-right">Pay Using Our Secure Gateway</li>
-            <li className="menu-list" data-aos="fade-right">
+            <li className="menu-list" data-aos="fade-up-right">Pick Your Favourite Creative Services</li>
+            <li className="menu-list" data-aos="fade-up-right">Pay Using Our Secure Gateway</li>
+            <li className="menu-list" data-aos="fade-up-right">
               Give Us A Brief About What You Need Done
             </li>
-            <li className="menu-list" data-aos="fade-right">
+            <li className="menu-list" data-aos="fade-up-right">
               Sit Back While We Deliver Your Creatives
             </li>
           </ul>
