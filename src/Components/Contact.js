8e8249ced09f4d14acab199ee8daea11eb83a8ab
georgejeswin <jeswinmyladoor@gmail.com>
2021-05-18 13:21:33 +0530

    navbar err fix

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import "./Contact.css";
-import Alert from '@material-ui/lab/Alert';
-import { CircularProgress } from '@material-ui/core'
+import Alert from "@material-ui/lab/Alert";
+import { CircularProgress } from "@material-ui/core";
 import $ from "jquery";
 window.$ = $;
 
 const Contact = () => {
-  const [alertInfo,setAlertInfo] = useState({
-    text:'',
-    type: ''
-  })
-  const [alert,setAlert] = useState(false)
-  const [loading,setLoading] = useState(false);
+  const [alertInfo, setAlertInfo] = useState({
+    text: "",
+    type: "",
+  });
+  const [alert, setAlert] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
@@ -19,7 +19,7 @@ const Contact = () => {
     message: "",
   });
   const handleSubmit = (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
     $.ajax({
       url: "https://script.google.com/macros/s/AKfycbycqvUnh14Jlpyc2sidbV0AVWVWt21AGGOtEhh_jiN0oL04BdEYPUxRdTGqOGOEsYs/exec",
@@ -32,22 +32,22 @@ const Contact = () => {
           phone: "",
           message: "",
         });
-        setLoading(false)
-        setAlert(true)
-        setAlertInfo({text:"form submitted",type:"success"});
+        setLoading(false);
+        setAlert(true);
+        setAlertInfo({ text: "form submitted", type: "success" });
         setInterval(() => {
-          setAlert(false)
-          setAlertInfo({})
+          setAlert(false);
+          setAlertInfo({});
         }, 4000);
       },
-      
+
       error: function (err) {
-        setLoading(false)
-        setAlert(true)
-        setAlert({text:"Something Error",type:"error"});;
+        setLoading(false);
+        setAlert(true);
+        setAlert({ text: "Something Error", type: "error" });
         setInterval(() => {
-          setAlert(false)
-          setAlertInfo({})
+          setAlert(false);
+          setAlertInfo({});
         }, 4000);
       },
     });
@@ -90,68 +90,72 @@ const Contact = () => {
           </div>
         </div>
       </div>
-   
-        <form
+
+      <form
         className="contact-form col-md-12 col-12 mt-5 pb-5"
         id="submit-form"
         action=""
         onSubmit={handleSubmit}
       >
         <h1>Message Us</h1>
-        {loading ? <CircularProgress  /> : alert ? <Alert severity={alertInfo.type}>{alertInfo.text}</Alert> : 
-        <div className="card card-2">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            id="name"
-            required
-            value={inputs.name}
-            onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            id="name"
-            required
-            value={inputs.email}
-            onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-          />
-          <input
-            type="number"
-            name="phone"
-            placeholder="Phone"
-            id="phone"
-            value={inputs.phone}
-            onChange={(e) => setInputs({ ...inputs, phone: e.target.value })}
-          />
-          <textarea
-            type="text"
-            rows="6"
-            placeholder="Messages"
-            name="message"
-            id="message"
-            required
-            value={inputs.message}
-            onChange={(e) => setInputs({ ...inputs, message: e.target.value })}
-          ></textarea>
-          <button type="submit" className="btn-2">
-            <span>Send</span>
-            <i className="fas fa-paper-plane"></i>
-          </button>
-          <div className="drops">
-            <div className="drop drop-5"></div>
-            <div className="drop drop-2"></div>
-            <div className="drop drop-7"></div>
-            <div className="drop drop-4"></div>
-            <div className="drop drop-6"></div>
+        {loading ? (
+          <CircularProgress />
+        ) : alert ? (
+          <Alert severity={alertInfo.type}>{alertInfo.text}</Alert>
+        ) : (
+          <div className="card card-2">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              id="name"
+              required
+              value={inputs.name}
+              onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              id="name"
+              required
+              value={inputs.email}
+              onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+            />
+            <input
+              type="number"
+              name="phone"
+              placeholder="Phone"
+              id="phone"
+              value={inputs.phone}
+              onChange={(e) => setInputs({ ...inputs, phone: e.target.value })}
+            />
+            <textarea
+              type="text"
+              rows="6"
+              placeholder="Messages"
+              name="message"
+              id="message"
+              required
+              value={inputs.message}
+              onChange={(e) =>
+                setInputs({ ...inputs, message: e.target.value })
+              }
+            ></textarea>
+            <button type="submit" className="btn-2">
+              <span>Send</span>
+              <i className="fas fa-paper-plane"></i>
+            </button>
+            <div className="drops">
+              <div className="drop drop-5"></div>
+              <div className="drop drop-2"></div>
+              <div className="drop drop-7"></div>
+              <div className="drop drop-4"></div>
+              <div className="drop drop-6"></div>
+            </div>
           </div>
-        </div>
-        }
-      </form> 
-      
-       
+        )}
+      </form>
     </div>
   );
 };
